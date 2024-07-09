4e16cbed72a79712453958154f6bc4a774c943b2
Silverlan <silverlann@gmail.com>
2024-07-09 21:43:35 +0200

    feat: add changes for core engine api changes

--- lua/entities/components/tf2_teamflag/shared.lua
+++ lua/entities/components/tf2_teamflag/shared.lua
@@ -1,4 +1,4 @@
-util.register_class("ents.tf2.Teamflag",BaseEntityComponent)
+util.register_class("ents.tf2.Teamflag", BaseEntityComponent)
 local Component = ents.tf2.Teamflag
 
 Component.GAME_TYPE_CTF = 0
@@ -8,11 +8,24 @@ Component.GAME_TYPE_INVADE = 3
 Component.GAME_TYPE_RESOURCE_CONTROL = 4
 Component.GAME_TYPE_ROBOT_DESTRUCTION = 5
 
-local noIoFlags = bit.bnot(bit.bor(ents.BaseEntityComponent.MEMBER_FLAG_BIT_INPUT,ents.BaseEntityComponent.MEMBER_FLAG_BIT_OUTPUT))
-if(SERVER) then
-	Component:RegisterMember("ReturnTime",ents.MEMBER_TYPE_FLOAT,60.0,{},bit.band(ents.BaseEntityComponent.MEMBER_FLAG_DEFAULT,noIoFlags))
+local noIoFlags =
+	bit.bnot(bit.bor(ents.BaseEntityComponent.MEMBER_FLAG_BIT_INPUT, ents.BaseEntityComponent.MEMBER_FLAG_BIT_OUTPUT))
+if SERVER then
+	Component:RegisterMember(
+		"ReturnTime",
+		ents.MEMBER_TYPE_FLOAT,
+		60.0,
+		{},
+		bit.band(ents.BaseEntityComponent.MEMBER_FLAG_DEFAULT, noIoFlags)
+	)
 end
-Component:RegisterMember("GameType",ents.MEMBER_TYPE_UINT32,Component.GAME_TYPE_CTF,{},bit.band(ents.BaseEntityComponent.MEMBER_FLAG_DEFAULT_NETWORKED,noIoFlags))
+Component:RegisterMember(
+	"GameType",
+	ents.MEMBER_TYPE_UINT32,
+	Component.GAME_TYPE_CTF,
+	{},
+	bit.band(ents.BaseEntityComponent.MEMBER_FLAG_DEFAULT_NETWORKED, noIoFlags)
+)
 
 local gmComponents = {
 	[Component.GAME_TYPE_CTF] = "gm_tf2_ctf",
@@ -20,7 +33,7 @@ local gmComponents = {
 	[Component.GAME_TYPE_TERRITORY_CONTROL] = "gm_tf2_territory_control",
 	[Component.GAME_TYPE_INVADE] = "gm_tf2_invade",
 	[Component.GAME_TYPE_RESOURCE_CONTROL] = "gm_tf2_resource_control",
-	[Component.GAME_TYPE_ROBOT_DESTRUCTION] = "gm_tf2_robot_destruction"
+	[Component.GAME_TYPE_ROBOT_DESTRUCTION] = "gm_tf2_robot_destruction",
 }
 
 function Component:__init()
@@ -31,33 +44,50 @@ function Component:Initialize()
 	self:AddEntityComponent(ents.COMPONENT_OWNABLE)
 	self:AddEntityComponent("tf2_carry_item")
 	local pickupC = self:AddEntityComponent("tf2_item_pickup")
-	if(pickupC ~= nil) then
+	if pickupC ~= nil then
 		pickupC:SetModel("flag/briefcase")
-		if(CLIENT) then pickupC:SetIdleAnimation("spin") end
+		if CLIENT then
+			pickupC:SetIdleAnimation("spin")
+		end
 	end
 
-	self:BindEvent(ents.OwnableComponent.EVENT_ON_OWNER_CHANGED,"OnOwnerChanged")
-	if(SERVER) then self:BindEvent(ents.tf2.ItemPickup.EVENT_ON_PICKED_UP,"OnPickedUp") end
+	self:BindEvent(ents.OwnableComponent.EVENT_ON_OWNER_CHANGED, "OnOwnerChanged")
+	if SERVER then
+		self:BindEvent(ents.tf2.ItemPickup.EVENT_ON_PICKED_UP, "OnPickedUp")
+	end
 end
-function Component:OnOwnerChanged(prevOwner,owner)
+function Component:OnOwnerChanged(prevOwner, owner)
 	local ent = self:GetEntity()
 	local physC = ent:GetComponent(ents.COMPONENT_PHYSICS)
-	if(util.is_valid(owner)) then
-		local attC = ent:AddComponent(ents.COMPONENT_ATTACHABLE)
-		local attInfo = ents.AttachableComponent.AttachmentInfo()
-		attInfo.flags = bit.bor(attInfo.flags,ents.AttachableComponent.FATTACHMENT_MODE_SNAP_TO_ORIGIN,ents.AttachableComponent.FATTACHMENT_MODE_UPDATE_EACH_FRAME,ents.AttachableComponent.FATTACHMENT_MODE_FORCE_IN_PLACE)
-		attC:AttachToAttachment(owner,"flag",attInfo)
-		if(CLIENT) then ent:PlayAnimation("idle") end
-		if(physC ~= nil) then physC:SetSimulationEnabled(false) end
+	if util.is_valid(owner) then
+		local attC = ent:AddComponent(ents.COMPONENT_ATTACHMENT)
+		local attInfo = ents.AttachmentComponent.AttachmentInfo()
+		attInfo.flags = bit.bor(
+			attInfo.flags,
+			ents.AttachmentComponent.FATTACHMENT_MODE_SNAP_TO_ORIGIN,
+			ents.AttachmentComponent.FATTACHMENT_MODE_UPDATE_EACH_FRAME,
+			ents.AttachmentComponent.FATTACHMENT_MODE_FORCE_IN_PLACE
+		)
+		attC:AttachToAttachment(owner, "flag", attInfo)
+		if CLIENT then
+			ent:PlayAnimation("idle")
+		end
+		if physC ~= nil then
+			physC:SetSimulationEnabled(false)
+		end
 		return
 	end
-	local attC = ent:GetComponent(ents.COMPONENT_ATTACHABLE)
-	if(attC ~= nil) then attC:ClearAttachment() end
-	if(CLIENT) then ent:PlayAnimation("spin") end
-	if(physC ~= nil) then
+	local attC = ent:GetComponent(ents.COMPONENT_ATTACHMENT)
+	if attC ~= nil then
+		attC:ClearAttachment()
+	end
+	if CLIENT then
+		ent:PlayAnimation("spin")
+	end
+	if physC ~= nil then
 		physC:SetSimulationEnabled(true)
 		physC:DropToFloor()
-		
+
 		local ang = ent:GetAngles()
 		ang.p = 0
 		ang.r = 0
@@ -66,10 +96,12 @@ function Component:OnOwnerChanged(prevOwner,owner)
 end
 function Component:OnEntitySpawn()
 	local gameType = self:GetGameType()
-	local entGm = ents.iterator({ents.IteratorFilterComponent(ents.COMPONENT_GAMEMODE)})()
-	if(entGm ~= nil) then
+	local entGm = ents.iterator({ ents.IteratorFilterComponent(ents.COMPONENT_GAMEMODE) })()
+	if entGm ~= nil then
 		local componentName = gmComponents[gameType]
-		if(componentName ~= nil) then entGm:AddComponent(componentName) end
+		if componentName ~= nil then
+			entGm:AddComponent(componentName)
+		end
 	end
 end
-ents.tf2.COMPONENT_TEAMFLAG = ents.register_component("tf2_teamflag",Component)
+ents.tf2.COMPONENT_TEAMFLAG = ents.register_component("tf2_teamflag", Component)
--- lua/weapons/components/tf2_weapon_bat/shared.lua
+++ lua/weapons/components/tf2_weapon_bat/shared.lua
@@ -1,4 +1,4 @@
-util.register_class("ents.tf2.WeaponBat",BaseEntityComponent)
+util.register_class("ents.tf2.WeaponBat", BaseEntityComponent)
 local Component = ents.tf2.WeaponBat
 
 include("sh_primary_attack.lua")
@@ -18,19 +18,19 @@ end
 
 function Component:Initialize()
 	local ent = self:GetEntity()
-	
-	self:AddEntityComponent(ents.COMPONENT_WEAPON,"InitializeWeapon")
-	self:AddEntityComponent(ents.COMPONENT_MODEL,"InitializeModel")
+
+	self:AddEntityComponent(ents.COMPONENT_WEAPON, "InitializeWeapon")
+	self:AddEntityComponent(ents.COMPONENT_MODEL, "InitializeModel")
 	self:AddEntityComponent(ents.COMPONENT_OWNABLE)
-	if(CLIENT == true) then
+	if CLIENT == true then
 		self:AddEntityComponent(ents.COMPONENT_SOUND_EMITTER)
-		self:BindEvent(ents.WeaponComponent.EVENT_ATTACH_TO_OWNER,"AttachToOwner")
+		self:BindEvent(ents.WeaponComponent.EVENT_ATTACH_TO_OWNER, "AttachToOwner")
 	end
-	if(SERVER == true) then
-		self:BindEvent(ents.OwnableComponent.EVENT_ON_OWNER_CHANGED,"OnOwnerChanged")
+	if SERVER == true then
+		self:BindEvent(ents.OwnableComponent.EVENT_ON_OWNER_CHANGED, "OnOwnerChanged")
 	end
-	self:BindEvent(ents.WeaponComponent.EVENT_ON_PRIMARY_ATTACK,"OnPrimaryAttack")
-	
+	self:BindEvent(ents.WeaponComponent.EVENT_ON_PRIMARY_ATTACK, "OnPrimaryAttack")
+
 	self:InitializePrimaryAttack()
 	self:InitializeSecondaryAttack()
 	self:InitializeReload()
@@ -39,33 +39,46 @@ end
 function Component:OnPrimaryAttack()
 	local ent = self:GetEntity()
 	local wepC = ent:GetComponent(ents.COMPONENT_WEAPON)
-	if(wepC == nil) then return end
+	if wepC == nil then
+		return
+	end
 
-	if(CLIENT == true) then
+	if CLIENT == true then
 		local sndEmitterComponent = ent:GetComponent(ents.COMPONENT_SOUND_EMITTER)
-		if(sndEmitterComponent ~= nil) then
-			sndEmitterComponent:EmitSound("weapons/bat_baseball_hit_world1.wav",sound.TYPE_WEAPON,ents.SoundEmitterComponent.SoundInfo(1.0,1.0)) -- TODO: Use sound script
+		if sndEmitterComponent ~= nil then
+			sndEmitterComponent:EmitSound(
+				"weapons/bat_baseball_hit_world1.wav",
+				sound.TYPE_WEAPON,
+				ents.SoundEmitterComponent.SoundInfo(1.0, 1.0)
+			) -- TODO: Use sound script
 		end
-		wepC:PlayViewActivity(Animation.ACT_MELEE_VM_HITCENTER,ents.AnimatedComponent.FPLAYANIM_RESET)
+		wepC:PlayViewActivity(Animation.ACT_MELEE_VM_HITCENTER, ents.AnimatedComponent.FPLAYANIM_RESET)
 	end
 
 	local owner = ent:GetOwner()
 	local animC = (owner ~= nil) and owner:GetComponent(ents.COMPONENT_ANIMATED) or nil
-	if(animC ~= nil) then
-		animC:PlayLayeredAnimation(0,"layer_melee_swing")
+	if animC ~= nil then
+		animC:PlayLayeredAnimation(0, "layer_melee_swing")
 	end
 
-	wepC:SetNextAttack(time.cur_time() +self.AttackDelay)
+	wepC:SetNextAttack(time.cur_time() + self.AttackDelay)
 end
 
-function Component:AttachToOwner(owner,vm)
+function Component:AttachToOwner(owner, vm)
 	local ent = vm and vm:GetEntity() or owner
-	local attC = self:AddEntityComponent(ents.COMPONENT_ATTACHABLE)
-	if(attC == nil) then return util.EVENT_REPLY_UNHANDLED end
-	local attInfo = ents.AttachableComponent.AttachmentInfo()
-	attInfo:SetRotation(EulerAngles(0,90,0):ToQuaternion())
-	attInfo.flags = bit.bor(attInfo.flags,ents.AttachableComponent.FATTACHMENT_MODE_SNAP_TO_ORIGIN,ents.AttachableComponent.FATTACHMENT_MODE_UPDATE_EACH_FRAME,ents.AttachableComponent.FATTACHMENT_MODE_FORCE_IN_PLACE)
-	attC:AttachToAttachment(ent,"weapon_bone",attInfo)
+	local attC = self:AddEntityComponent(ents.COMPONENT_ATTACHMENT)
+	if attC == nil then
+		return util.EVENT_REPLY_UNHANDLED
+	end
+	local attInfo = ents.AttachmentComponent.AttachmentInfo()
+	attInfo:SetRotation(EulerAngles(0, 90, 0):ToQuaternion())
+	attInfo.flags = bit.bor(
+		attInfo.flags,
+		ents.AttachmentComponent.FATTACHMENT_MODE_SNAP_TO_ORIGIN,
+		ents.AttachmentComponent.FATTACHMENT_MODE_UPDATE_EACH_FRAME,
+		ents.AttachmentComponent.FATTACHMENT_MODE_FORCE_IN_PLACE
+	)
+	attC:AttachToAttachment(ent, "weapon_bone", attInfo)
 	return util.EVENT_REPLY_HANDLED
 end
 
@@ -74,18 +87,18 @@ function Component:InitializeModel(component)
 end
 
 function Component:InitializeWeapon(component)
-	if(CLIENT == true) then
+	if CLIENT == true then
 		-- component:SetViewModel(self.ViewModel)
 		-- component:SetViewFOV(self.ViewFOV)
 	end
 	component:SetAutomaticPrimary(self.Primary.Automatic)
 	component:SetAutomaticSecondary(self.Secondary.Automatic)
 
-	if(SERVER == true) then
+	if SERVER == true then
 		component:SetPrimaryAmmoType(self.Primary.AmmoType)
 		component:SetMaxPrimaryClipSize(self.Primary.MaxClipSize)
-		
+
 		component:SetSecondaryAmmoType(self.Secondary.AmmoType)
 	end
 end
-ents.tf2.COMPONENT_TF2_WEAPON_BAT = ents.register_component("tf2_weapon_bat",Component)
+ents.tf2.COMPONENT_TF2_WEAPON_BAT = ents.register_component("tf2_weapon_bat", Component)
